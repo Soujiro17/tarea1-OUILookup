d6fb630e8dd9aa10bb7cd339aa715abfaa93181b
Vicente Reyes <vicente_tomi@hotmail.com>
2020-11-06 11:11:36 -0300

    implementada la busqueda por ip de la mac address

---
+++ OUILookup.py
@@ -0,0 +1,32 @@
+# Imports
+from getmac import get_mac_address
+import argparse
+import warnings
+
+# ARGP
+parser = argparse.ArgumentParser()
+
+# Configuración parámetros
+parser.add_argument("--ip", help="specify the IP of the host to query.")
+parser.add_argument("--mac", help="specify the MAC address to query.")
+
+# Cerrar
+args = parser.parse_args()
+
+# Condiciones de None
+if(args.ip != None):
+    eth_mac = get_mac_address(ip=args.ip)
+    if(eth_mac != None):
+        print(f'MAC address  : {eth_mac}\nVendor       : hola hola hola')
+    else:
+        print("Error: ip is outside the host network")
+
+if(args.mac != None):
+    pass
+
+    #win_mac = get_mac_address(interface="Ethernet 1")
+    #ip_mac = get_mac_address(ip="192.168.0.1")
+    #ip6_mac = get_mac_address(ip6="::1")
+    #host_mac = get_mac_address(hostname="localhost")
+    #updated_mac = get_mac_address(ip="10.0.0.1", network_request=True)
+    # print(f'{eth_mac}\n{win_mac}\n{ip_mac}\n{ip6_mac}\n{host_mac}\n{updated_mac}\n')
